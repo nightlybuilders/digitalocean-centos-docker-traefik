64c525f8d3f352b7ab29c3a6b4ce59a9914fb167
Lukas Ender <hello@lukasender.at>
2018-10-28 13:24:15 +0100

    Basic Traefik configuration

--- fabfile/stacks/infra/__init__.py
+++ fabfile/stacks/infra/__init__.py
@@ -8,24 +8,54 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 @task
 def setup(c):
+    c.run('mkdir -p docker/infra')
     """Upload the docker-compose.yml"""
     c.put(f'{HERE}/docker-compose.yml',
-          remote='docker-compose.yml')
+          remote='docker/infra/docker-compose.yml')
+    c.put(f'{HERE}/traefik.toml',
+          remote='docker/infra/traefik.toml')
+    c.put(f'{HERE}/dot.env',
+          remote='docker/infra/.env')
+    
+    output = c.run('docker network ls | grep proxy')
+    if not 'proxy' in output.stdout:
+        c.run('docker network create proxy')
+
+    c.run('test -f docker/infra/acme.json || touch docker/infra/acme.json')
+    c.run('test -f docker/infra/acme.json || chmod 600 docker/infra/acme.json')
 
 
 @task
 def up(c):
     """docker-compose up -d"""
-    c.run('docker-compose -f ~/docker-compose.yml up -d')
+    with c.cd('~/docker/infra'):
+      c.run('docker-compose config')
+      c.run('docker-compose up -d')
 
 
 @task
 def down(c):
     """docker-compose down"""
-    c.run('docker-compose -f ~/docker-compose.yml down')
+    with c.cd('~/docker/infra'):
+      c.run('docker-compose down')
 
 
 @task
 def ps(c):
     """docker-compose ps"""
-    c.run('docker-compose -f ~/docker-compose.yml ps')
+    with c.cd('~/docker/infra'):
+      c.run('docker-compose ps')
+
+
+@task
+def config(c):
+    """docker-compose config"""
+    with c.cd('~/docker/infra'):
+      c.run('docker-compose config')
+
+
+@task
+def restart(c, container=''):
+    """docker-compose restart <container>"""
+    with c.cd('~/docker/infra'):
+      c.run(f'docker-compose restart {container}')
